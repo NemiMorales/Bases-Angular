0b802b61f4a1a66b489fbb1489748e7a3df45006
Noemi <belenemi.ms@gmail.com>
2023-08-28 09:53:11 -0400

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { CounterModule } from './counter/couter.module';
 import { HeroesModule } from './heroes/heroes.module';
 import { DsnModule } from './dsn/dsn.module';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './gifs/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SlidebarComponent,
+    SidebarComponent,
+    HomeComponent,
 
   ],
   imports: [
